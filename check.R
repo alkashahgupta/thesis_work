308bb141b55ba9fa89f35d88711cf2cb1232f04c
Alka Shah <alkashah.gupta@gmail.com>
2012-10-31 02:34:35 +0700

    add new file

--- check.R
+++ check.R
@@ -36,3 +36,94 @@ While(N>=0)
 	}
 }
 names<- get.vertex.attribute(ful_n_frame, "name")
+
+
+
+
+
+
+To find common path 
+for (k=1, k<= numobs-1, k++)
+	for(i=1,i<=numobs-1, i++)
+		{
+			if (k==i)
+				{
+					a<- father(o1)
+					b<- father(o(k+1))
+					path =0
+
+					if(a==b)
+					{
+						path=2*path
+					}
+
+					else
+					{
+
+
+					}
+
+
+				}
+
+			else
+			{
+				p=k
+				a<-bfs$dist[o1]
+				while(p!=i)
+				{					
+					b<-bfs$dist[o(p+1)]					
+					if(a<=b)
+						{
+							diff<- b-a
+							while (diff!=0)
+							{
+								node_b<- father(o(p+1))
+								o(p+1)<- node_b
+								diff--
+							}
+			
+							if (father[o1]==father[o(p+1)])
+							{
+								commanfather[p]<-father[o1]
+							}
+			
+							else
+							{
+								q<-father[o1]
+								L<- father[o(p+1)]
+								m<-0
+								while(q!=L)
+								{
+									o1<-q
+									o(p+1)<-L
+									q<-father[o1]
+									L<-father[o(p+1)]
+									m++
+								}
+								commanfather[p]<-q
+							}
+
+						}
+					p=i
+				}
+							
+			}
+		}  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
