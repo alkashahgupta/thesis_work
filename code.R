8c02cc188cddfce5a76c5641d39ad6669bc80894
Alka Shah <alkashah.gupta@gmail.com>
2012-10-30 15:28:54 +0700

    add new file

--- code.R
+++ code.R
@@ -8,3 +8,6 @@ ful_n_frame<- graph.data.frame(fulnetwork)
 
 N<- vcount(ful_n_frame)
 
+names<- get.vertex.attribute(ful_n_frame, "name")
+ bfs<-graph.bfs (ful_n_frame, root=1, order=TRUE, rank=TRUE, father=TRUE, pred=TRUE, succ=TRUE, dist=TRUE,unreachable= FALSE)
+
