8de2418be2c63e624af952c559a3860fbe4a54cd
Alka Shah <alkashah.gupta@gmail.com>
2012-11-02 17:03:17 +0700

    add new file

--- check.R
+++ check.R
@@ -21,18 +21,47 @@ N<- vcount(ful_n_frame)
 				}
 				for(ob=1,ob<=obsnum,ob++)
 				{					
-					detmean[ob]<-Mean*(obsdist[ob+1]-obsdist[ob])
+					detmean[ob][1]<-Mean*(obsdist[ob+1]-obsdist[ob])
 					for(var=1,var<=obsnum,var++)
 						{
 							covarance[ob][var]<-stddev*stddev*commonpath(ob,var)#here i need function to find common path between observers
 						}					
 				
 				}
-	
+				for(i=0,i<k-1,i++)
+					for(j=0,j<1,j++) 
+					{
+						detmean_Transpose[i][j]<-detmean[j][i]
+					}
+
+				for (i=0;i<1;i++)
+					   for(j=0;j<k;j++)
+					   {
+					  	pro_detmean_covariance[i][j]=0;
+					  for(p=0;p<k;p++)
+						  pro_detmean_covariance[i][j]+= detmean_Transpose[i][p]*covariance[p][j];
+						}
+				for (i=0;i<k;i++)
+					   for(j=0;j<1;j++)
+					   {
+					  	sub_detdelay_detmean[i][j]=0;
+					    sub_detdelay_detmean[i][j]= detdelay[i][j]-(0.5*detmean[i][j])
+						}
+
+				for (i=0;i<1;i++)
+					   for(j=0;j<1;j++)
+					   {
+					  	source[i][j]=0;
+					  for(p=0;p<k;p++)
+						  source[i][j]+= pro_detmean_covariance[i][p]*sub_detdelay_detmean[p][j];
+						}
+
 	}
 
 names<- get.vertex.attribute(ful_n_frame, "name")
 
+
+         
 
 
 
