b2c1374e5f989802688e834174b5cb2208090a86
Alka Shah <alkashah.gupta@gmail.com>
2012-11-02 13:50:10 +0700

    add new file

--- check.R
+++ check.R
@@ -24,9 +24,7 @@ N<- vcount(ful_n_frame)
 					detmean[ob]<-Mean*(obsdist[ob+1]-obsdist[ob])
 					for(var=1,var<=obsnum,var++)
 						{
-							if(ob==var)
-								{							
-								covarance[ob][var]<-stddev*stddev*obs#here i need function to find common path between observers
+							covarance[ob][var]<-stddev*stddev*commonpath(ob,var)#here i need function to find common path between observers
 						}					
 				
 				}
@@ -39,53 +37,6 @@ names<- get.vertex.attribute(ful_n_frame, "name")
 
 
 
-
-# To find common path for covariance  
-for (k=1, k<= numobs-1, k++)
-	for(i=1,i<=numobs-1, i++)
-		{
-			if (k==i)#it give path between reference observer and other observers
-				{
-					a<- father(o1)
-					b<- father(o(k+1))
-					path=0
-
-					if(a==b)
-					{
-						path=2
-						
-					}
-
-					else
-					{see which is more farther than root
-					then keep them in one level going up and counting the move too say p
-					see if they have common father
-					if not then move up to have common father and count the move simaltaneously say q
-					finally path =  p +2q
-					 
-
-
-					}
-
-
-				}
-
-			else#here we will find the common path between two pairs of reference observer and other observers
-			{			
-				c_father_o1_o2<- common_father(o1,o2)
-				c_father_o1_o3<- common_father(o1,o3)
-				c_father_o2_o3<- common_father(o2,o3)
-					if(c_father_o1_o2==c_father_o1_o3)
-					{
-						
-						path= bfs$dist[o1]-bfs$dist[commonfather either i.e c_father_o1_o2 or c_father_o1_o3] +bfs$dist(c_father_o2_o3)-bfs$dist[commonfather either i.e c_father_o1_o2 or c_father_o1_o3] #-ve distance of common father if common father is not root. 
-					}
-					else
-					{
-						path= bfs$dist[o1]-mod(bfs$dist[c_father_o1_o2]-bfs$dist[c_father_o1_o3])
-					}		
-			}
-		}  
 
 
 
