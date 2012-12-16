fa970a78f1786c4f31bc6576f3f2141599f41bdf
Alka Shah <alkashah.gupta@gmail.com>
2012-12-16 08:55:08 +0700

    add new file

--- code.R
+++ code.R
@@ -1,35 +1,41 @@
 obs_meas<-read.csv ("measure1.csv")
 obs_meas_df<-data.frame(obs_meas)
 fulnetwork<- read.csv("relations bimode simplified max1.csv", header = TRUE,sep = "\t")
-obsnum<-3
+
 Mean<-sapply(obs_meas_df, mean)
 varianc <- var(obs_meas_df)
 #print(varianc)
 #mintime <- sapply(obs_meas_df, min)
+
+obsnum<-3
+
+#variable intialization as a list
 d<-list()
-for(i in seq(1:(obsnum-1)))
-{
-d[i]<-obs_meas_df[i+1,1]-obs_meas_df[1,1]
-}
-#d<-obs_meas_df-mintime
-library(igraph)
-ful_n_frame<- graph.data.frame(fulnetwork,directed=FALSE)
-# plot(ful_n_frame)
-
-N<- vcount(ful_n_frame)
-
-
-names<- get.vertex.attribute(ful_n_frame, "name")
-a<-1
 s<-list()
 obsdist<-list()
 detmean<-list()
 covarance<-list()
 
+
+#calculation of delay vector
+for(i in seq(1:(obsnum-1)))
+{
+d[i]<-obs_meas_df[i+1,1]-obs_meas_df[1,1]
+}
+
+library(igraph)
+ful_n_frame<- graph.data.frame(fulnetwork,directed=FALSE)
+# plot(ful_n_frame)
+
+N<- vcount(ful_n_frame)
+names<- get.vertex.attribute(ful_n_frame, "name")
 length(obsdist)<-obsnum
+
+#observer id are to be entered here
 b<-c(6,7,8)
 o1<-b[1]
 
+#function to find common father
 common_father<-function(obs1,obs2)
 				{
 					o2<-obs1
@@ -87,7 +93,9 @@ common_father<-function(obs1,obs2)
 					
 					}
 
-commonpathe <- function(k,l) #we don't need any information of observer.Simply the value of k and i which is used as intforpositionof matrix 
+
+#function to find common path
+commonpathe <- function(k,l) #we don't need any information of observer.Simply the value of k and i 								which is used as intforpositionof matrix 
 {
 			
 				if (k==l)#it give path between reference observer and other observers
@@ -140,45 +148,44 @@ commonpathe <- function(k,l) #we don't need any information of observer.Simply t
 
 
 
-
+#main body that uses common father and common path function
 
  for(i in seq(1:N))
 	{
-	if(i==b[1]|i==b[2]|i==b[3])
-	{
-	next
-	}
-
-	print(i)
-	
-	bfs<-graph.bfs (ful_n_frame, root=i, order=TRUE, rank=TRUE, father=TRUE, pred=TRUE, succ=TRUE, dist=TRUE,unreachable= FALSE)
-	#print(bfs$dist[bfs$order[2]])
-	for(ob in seq(1:obsnum))
-		{b<-c(6,7,8)
-			obsdist[ob]<-bfs$dist[b[ob]]
-#print(obsdist[ob])				
+		if(i==b[1]|i==b[2]|i==b[3])
+		{
+		next
 		}
-w<-1
-for(ob in seq(1:(obsnum-1)))
-				{					
-					obsdist_as_num<-as.numeric(obsdist)					
-					detmean[ob]<-Mean*(obsdist_as_num[ob+1]-obsdist_as_num[1])
+
+		print(i)
+	
+		bfs<-graph.bfs (ful_n_frame, root=i, order=TRUE, rank=TRUE, father=TRUE, pred=TRUE, succ=TRUE, dist=TRUE,unreachable= FALSE)
+#print(bfs$dist[bfs$order[2]])
+		for(ob in seq(1:obsnum))
+			{b<-c(6,7,8)
+				obsdist[ob]<-bfs$dist[b[ob]]
+#print(obsdist[ob])				
+			}
+		w<-1
+		for(ob in seq(1:(obsnum-1)))
+			{					
+				obsdist_as_num<-as.numeric(obsdist)					
+				detmean[ob]<-Mean*(obsdist_as_num[ob+1]-obsdist_as_num[1])
 										
 				for(varim in seq(1:(obsnum-1)))
-						{
+					{
 							
-							temp<-commonpathe(ob,varim)#see here
+						temp<-commonpathe(ob,varim)#see here
 #print(temp)
-							covarance[w]<-varianc*temp#here i need function to find common path between observers
+						covarance[w]<-varianc*temp#here i need function to find common path between observers
 #print(covarance[w])							
-w<-w+1
+						w<-w+1
 							
 
-						}
-				}
+					}
+			}
+
 		matrix_order<-obsnum-1
-
-
 		mat1<-matrix(covarance,matrix_order,matrix_order)
 		mat2<-matrix(detmean, matrix_order)
 		mat3<-matrix(d,matrix_order)
@@ -191,6 +198,7 @@ w<-w+1
 
 
 	}
+
 s_sort<-sort(as.numeric(s), index.return = TRUE)
 s_node<-s_sort$ix[1]
 
