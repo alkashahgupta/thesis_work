ef5e8e85720d7aea49fc22e79f1faec9da4605a0
Alka Shah <alkashah.gupta@gmail.com>
2013-01-13 22:22:26 +0700

    add new file

--- igrap.R
+++ igrap.R
@@ -1,7 +1,8 @@
 library(igraph)
-newa<- read.csv("Random01.csv", header = TRUE,sep = ",")
+newa<- read.csv("SmallWorld01.csv", header = TRUE,sep = ",")
  aja <- data.frame(newa$Node,newa$Source)
 ajau<-graph.data.frame(aja,directed=FALSE)
-pt<-c(0,4,44,92)
+names<- get.vertex.attribute(ajau, "name")
+pt<-c("59")
 V(ajau)[pt]$color<-"Green"
-plot(ajau,vertex.size=3)
+plot(ajau,vertex.size=3,vertex.label.dist=0.3,vertex.label=names)
