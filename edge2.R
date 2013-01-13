54a2c8282f4648a7930870503cbc539131e8789b
Alka Shah <alkashah.gupta@gmail.com>
2013-01-14 02:33:59 +0700

    add new file

--- edge2.R
+++ edge2.R
@@ -1,8 +1,14 @@
+propcsv<-read.csv ("propdelay.csv")
+propcsvdf<-data.frame(propcsv)
+meand<-read.csv("mean.csv")
+meanddf<-data.frame(meand)
+covd<-read.csv("covar.csv",sep=",",header = FALSE)
+covddf<-data.frame(covd)
 en<- ecount(ful_n_frame)
 vm<-obsnum-1
 vn<-en
 atest<- array(list(),c(vm,vn))
-cm<-c(1,1,1,1)
+cm<-c(0,1,1,1)
 btest<-array(cm,c(2,2))
 for(vh in seq(1:1))
 {
@@ -64,5 +70,37 @@ atest[vi,aaintr[ack]]<-0
 			
 		}
 	}
-print(atest)
+for(i in 1:vm)
+{
+for(j in 1:vn)
+{
+
+if(atest[i,j]=="NULL")
+{
+atest[i,j]<-0
+}
+}
+}
+#print(atest)
+matatest<-matrix(atest,2)
+#edetdelay<-matrix(lapply(matatest,function(x) x* Mean),2) #deterministic delay
+matmean<-as.matrix(meanddf$propdelay)
+matcovar<-as.matrix(covddf)
+edetdelay<-matrix(as.numeric(matatest),obsnum-1) %*% matmean
+atest_tranpose<-t(matatest)
+
+#ecov<-matrix(lapply(matatest,function(x) x * varianc) ,2)
+ecov<-matrix(as.numeric(matatest),obsnum-1) %*% matcovar
+matxy<-matrix((as.numeric(ecov)),2)
+matyy<-matrix((as.numeric(atest_tranpose)),en)
+matcov<-matxy %*% matyy #covariance
+matprop<-as.matrix(propcsvdf$propdelay)
+
+exp_d<-matrix(as.numeric(matatest),2) %*% matprop
+#we have function exp for exponential and det... for determinant which is used to divide the result of exponential values in estimator
+
+
+
+ 
+
 }
