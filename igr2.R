466be8eb0a58364e74eb4ce29cc19049e1e0b698
Alka Shah <alkashah.gupta@gmail.com>
2013-01-21 02:00:25 +0700

    add new file

--- igr2.R
+++ igr2.R
@@ -1,30 +1,61 @@
 library(igraph)
-newa<- read.csv("SmallWorldGraphMatrix.csv", header = FALSE,sep = ",")
+library(plotrix)
+newa<- read.csv("RandomGraphMatrix.csv", header = FALSE,sep = ",")
  #aja <- data.frame(newa$Node,newa$Source)
 #ajau<-graph.data.frame(aja,directed=FALSE)
 ajau<-graph.adjacency(newa,mode="undirected")
 names<- get.vertex.attribute(ajau, "name")
-names_1<-signif(unlist(s),3)
-pt<-c(10)
-ob<-c( 58 ,31 ,84 ,12)
-fs<-c(7)
-pg<-get.shortest.paths(ful_n_frame,10,7,mode="all",output="epath")
+testval<-round(unlist(s))
+testcol<-rep(0,length(testval))
+testcol[testval<0]<-color.scale(testval[testval<0],0,c(0,1),c(1,0),color.spec="rgb")
+testcol[testval>=0]<-color.scale(testval[testval>=0],1,c(1,0),0,color.spec="rgb")
+#plot(testval,col=testcol,pch=19)
+
+pt<-c(82)
+ob<-c(  36 ,73 ,97 , 2   )
+fs<-c(12)
+pg<-get.shortest.paths(ful_n_frame,82,12,mode="all",output="epath")
 as<-unlist(pg)
 E(ajau)$color<-"grey"
  E(ajau)[as]$color<-"red"
-colbar<- heat.colors(83)
+colbar<- heat.colors(73)
 #V(ajau)$color<-colbar[(names_1)]
 #V(ajau)[pt]$color<-"Green"
-V(ajau)[fs]$label.color<-"red"
-V(ajau)[ob]$label.color<-"blue"
+V(ajau)[fs]$label.color<-"Blue"
+V(ajau)[ob]$label.color<-"red"
 V(ajau)[pt]$label.color<-"Green"
-#V(ajau)[82]$frame.color<- "red"
+#V(ajau)[pt]$label<-"a"
+#V(ajau)[fs]$frame.color<-"blue"
+#V(ajau)[ob]$frame.color<-"red"
+#V(ajau)[pt]$frame.color<-"Green"
+#V(ajau)[82]$frame.color<- "Green"
 #V(ajau)[fs]$color<-"Yellow"
-par(mar=c(2.1,2.1,2.1,2.1))
+#bottom,left,top, right
+par(mar=c(2.1,13.1,2.1,2.1))
+#morefakedata<-abs(names_1)
+#fakemax <-max(morefakedata)
+#pal2 <-colorRampPalette(c("red","white","Blue"))
+#numshades <-25
+#colors2 <-pal2(numshades)
+# Make chart with color ramp
+#morecolors <-c()
+#colindex<-c()
+#for (i in 1:length(morefakedata)) {
+#colindex[i] <-round((
+#morefakedata[i] / fakemax) * numshades)
+#morecolors <-c(
+#morecolors, colors2[colindex[1]])
+#}
 
-png(file= "sw.02.01", height=700, width=700,bg="white" )
-plot(ajau,vertex.size=3,vertex.label.dist=0.35,vertex.label=names_1,vertex.color=colbar[round((abs(names_1)+1)*10)] ,vertex.label.font=2,vertex.label.cex=1.3,edge.width=1.5 )
-legend("topright", inset=.0,c("Orignal Source","Observers","Predicted Source","Normal Nodes"), fill=c("Green","Blue","Red","Black"), horiz=FALSE)
-title(main="Small-World Graph",xlab="Nodes Color varies with estimator value and label based on type of nodes(83)",cex.lab=1.3,col.lab="Black")
+png(file= "rn.04.01.png", height=700, width=700,bg="white" )
+plot(ajau,vertex.size=4,vertex.label.dist=0.45,vertex.label=testval,vertex.color=testcol,
+vertex.label.font=2,vertex.label.cex=1.3,edge.width=1.5 )
+#nodelabels(fs, 4)
+legend("topright", inset=.0,c("Orignal Source","Observers","Predicted Source"), fill=c("Green","Red","Blue"), horiz=FALSE)
+legend("topleft","x,y",unique(testval),fill=unique(testcol))
+#legend("topleft","x,y",sort(unique(testval), decreasing = FALSE),fill=sort(unique(testcol), decreasing = TRUE))
+title(main="Random Graph",xlab="Nodes Color varies with estimator value and its label based on type of nodes(72)",cex.lab=1.3,col.lab="Black")
 
 dev.off()
+
+
