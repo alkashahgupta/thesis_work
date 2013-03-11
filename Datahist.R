9a023b7180a68a786c44a45f76c542e68d30c7a7
Alka Shah <alkashah.gupta@gmail.com>
2013-03-11 23:45:05 +0700

    add new file

--- Datahist.R
+++ Datahist.R
@@ -1,22 +1,19 @@
-png(file= "6swhd.png", height=700, width=700,bg="white" )
-
-par(mfrow=c(3,2))
-Pathinfo<-read.csv("data.csv",header=T,sep=",")
-#Pathinfosf<-read.csv("4obsfsirmean.csv",header=T,sep=",")
-#Pathinfosw<-read.csv("4obswsirmean.csv",header=T,sep=",")
-# barplot(Pathinfo, main="Network", ylab= "Total", beside=TRUE, col=rainbow(10))
 #Hoperror <- c(Pathinfo$Random, Pathinfo$ScaleFree, Pathinfo$Smallworld)
 #hist(Hoperror, col="lightblue", freq=FALSE)
 #dotchart(t(Pathinfo), color=c("red","blue","darkgreen"),main="Dotchart for Autos", cex=0.8)
-colsw<-c("red", "yellow", "green", "violet", "orange", "blue", "pink", "cyan", "Black","white")
-colsf<-c("red","yellow", "green", "violet", "orange", "blue", "pink", "cyan","black","white")
-colrn<-c( "red","yellow", "green", "violet", "orange", "blue", "pink", "cyan","black","white")
-hist(Pathinfo$SmallWorld12, col=colsf, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "5obs with higher degree",xlab="Hop error",ylab= "Percentage")
-hist(Pathinfo$SmallWorld13, col=colsf, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "10obs with higher degree",xlab="Hop error",ylab= "Percentage")
-hist(Pathinfo$SmallWorld15, col=colsf, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "15obs with higher degree",xlab="Hop error",ylab= "Percentage")
-hist(Pathinfo$SmallWorld17, col=colsf, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "20obs with higher degree",xlab="Hop error",ylab= "Percentage")
+#colsw<-c("red", "yellow", "green", "violet", "orange", "blue", "pink", "cyan", "Black","white")
+png(file= "swginvhd.png", height=700, width=700,bg="white" )
+par(mfrow=c(2,2),oma=c(0,0,2,0))
+Pathinfo<-read.csv("data.csv",header=T,sep=",")
 
+hist(Pathinfo$Sw33, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "5 observers ",xlab="Hop error",ylab= "Percentage")
 
+hist(Pathinfo$Sw35, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "10 observers ",xlab="Hop error",ylab= "Percentage")
 
+hist(Pathinfo$Sw38, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "15 observers ",xlab="Hop error",ylab= "Percentage")
 
+hist(Pathinfo$Sw40, freq=FALSE,breaks=c(0:10),ylim=c(0,1),labels = TRUE, xlim=c(0,10),include.lowest=TRUE,right = FALSE,main = "20 observers ",xlab="Hop error",ylab= "Percentage")
+
+#title(" Hop Error of Small-World Network based on Number of Observers taken Randomly", cex.main=1.5, outer=TRUE)
+title(" Hop Error of Small-World Network based on Number of Observers with Higher Degree", cex.main=1.5,outer=TRUE)
 dev.off()
